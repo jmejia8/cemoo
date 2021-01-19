311e213a9fbf247694fc5b13d69a2cd141347c7e
Jesús Mejía <jesusmejded@gmail.com>
2021-01-18 21:43:56 -0600

    minor improvements

--- get-no-dominated.c
+++ get-no-dominated.c
@@ -1,91 +1,7 @@
 #include "utils.c"
 #include "sort.c"
 
-// is v dominated by u?
-int is_dominated(float* v, float* u, int n){
-    int i, flag = 0;
-    for (i = 0; i < n; ++i) {
-        if (u[i] > v[i]) {
-           return 0; 
-        } else if (u[i] < v[i]){
-            flag = 1;
-        }
-    }
 
-    return flag;
-}
-
-
-int is_non_dominated(float* sol_a, float* sol_b, int n){
-    return !is_dominated(sol_a, sol_b, n);
-}
-
-void method_one(float* fvalues, int* non_dominated, int pop_size, int* n_non_dominated, int n){
-    int i=0, j, k=0;
-
-    for (i = 0; i < pop_size; ++i) {
-        for (j = 0; j < pop_size; ++j) {
-            if (i == j) continue;
-            if (is_dominated(&fvalues[i*n], &fvalues[j*n], n))
-               break; 
-        }
-
-        // if i is non-dominated by any in population
-        if (j >= pop_size) 
-            non_dominated[k++] = i;
-    }
-
-    *n_non_dominated = k;
-}
-
-void deleteat(int* array, int len, int i) {
-    int j;
-
-    for (j = i; j < len-1; ++j) {
-        array[j] = array[j+1];
-    }
-    
-}
-
-void continous_update(float* fvalues, int* non_dominated, int* mask, int pop_size, int len_mask, int* n_non_dominated, int n){
-    int i, j, len_p=1;
-    int jj, ii;
-
-    // step 1
-    non_dominated[0] = mask[0];
-
-    for (i = 1; i < len_mask; ++i) {
-        j = 0;
-        ii = mask[i];
-        while ( j < len_p ) {
-            jj = non_dominated[j];
-            if(is_dominated(&fvalues[ii*n], &fvalues[jj*n], n)){
-                break;
-            }
-
-            if (is_dominated(&fvalues[jj*n], &fvalues[ii*n], n)){
-                deleteat(non_dominated, len_p, j);
-                jj = non_dominated[j];
-                --len_p;
-                continue;
-            }
-            ++j;
-        }
-
-        // if i is non-dominated by any in population
-        if (j >= len_p) 
-            non_dominated[len_p++] = ii;
-    }
-
-    *n_non_dominated = len_p;
-}
-
-void method_two(float* fvalues, int* non_dominated, int pop_size, int* n_non_dominated, int n){
-    int i;
-    int* mask = ivector(pop_size);
-    for (i = 0; i < pop_size; ++i) mask[i] = i;
-    continous_update(fvalues, non_dominated, mask, pop_size, pop_size, n_non_dominated, n);
-}
 /*
  * a and b are vectors with size k
  * 'i' means a and b are incomparable
@@ -115,6 +31,95 @@ char compare(float* a, float* b, int k)
     return 'D'; // b dominates a
     
 }
+
+// is v dominated by u?
+int is_dominated(float* v, float* u, int n){
+    int i, flag = 0;
+    for (i = 0; i < n; ++i) {
+        if (u[i] > v[i]) {
+           return 0; 
+        } else if (u[i] < v[i]){
+            flag = 1;
+        }
+    }
+
+    return flag;
+}
+
+
+int is_non_dominated(float* sol_a, float* sol_b, int n){
+    return !is_dominated(sol_a, sol_b, n);
+}
+
+void method_one(float* fvalues, int* non_dominated, int pop_size, int* n_non_dominated, int n){
+    int i=0, j, k=0;
+
+    for (i = 0; i < pop_size; ++i) {
+        for (j = 0; j < pop_size; ++j) {
+            if (i == j) continue;
+            if (compare(&fvalues[i*n], &fvalues[j*n], n) == 'D')
+               break; 
+        }
+
+        // if i is non-dominated by any in population
+        if (j >= pop_size) 
+            non_dominated[k++] = i;
+    }
+
+    *n_non_dominated = k;
+}
+
+void deleteat(int* array, int len, int i) {
+    int j;
+
+    for (j = i; j < len-1; ++j) {
+        array[j] = array[j+1];
+    }
+    
+}
+
+void continous_update(float* fvalues, int* non_dominated, int* mask, int pop_size, int len_mask, int* n_non_dominated, int n){
+    int i, j, len_p=1;
+    int jj, ii;
+    char relation;
+
+    // step 1
+    non_dominated[0] = mask[0];
+
+    for (i = 1; i < len_mask; ++i) {
+        j = 0;
+        ii = mask[i];
+        while ( j < len_p ) {
+            jj = non_dominated[j];
+            relation = compare(&fvalues[ii*n], &fvalues[jj*n], n);
+            if(relation == 'D'){
+                break;
+            }
+
+            if (relation == 'd'){
+                deleteat(non_dominated, len_p, j);
+                jj = non_dominated[j];
+                --len_p;
+                continue;
+            }
+            ++j;
+        }
+
+        // if i is non-dominated by any in population
+        if (j >= len_p) 
+            non_dominated[len_p++] = ii;
+    }
+
+    *n_non_dominated = len_p;
+}
+
+void method_two(float* fvalues, int* non_dominated, int pop_size, int* n_non_dominated, int n){
+    int i;
+    int* mask = ivector(pop_size);
+    for (i = 0; i < pop_size; ++i) mask[i] = i;
+    continous_update(fvalues, non_dominated, mask, pop_size, pop_size, n_non_dominated, n);
+}
+
 
 void bentleys_method(float* fvalues,
                      int* non_dominated,
