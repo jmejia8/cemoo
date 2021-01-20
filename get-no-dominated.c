09b55e7313636b8b6af62293272433d41a22c13a
Jesus Mejia <jesusmejded@gmail.com>
2021-01-19 18:33:44 -0600

    salida para ejecución a mano

--- get-no-dominated.c
+++ get-no-dominated.c
@@ -49,18 +49,41 @@ int is_dominated(float* v, float* u, int n){
 
 void method_one(float* fvalues, int* non_dominated, int pop_size, int* n_non_dominated, int n){
     int i=0, j, k=0;
+    int l;
 
+
+    printf("Paso 1: $i = %d$. $P' = \\emptyset$\n\n", i+1);
     for (i = 0; i < pop_size; ++i) {
         for (j = 0; j < pop_size; ++j) {
             if (i == j) continue;
-            if (compare(&fvalues[i*n], &fvalues[j*n], n) == 'D')
-               break; 
+            printf("Paso 2: Para $j = %d$. ", j+1);
+            if (compare(&fvalues[i*n], &fvalues[j*n], n) == 'D'){
+                printf("Como $j = %d$ domina a $i = %d$, ir a Paso 4\n\n", j+1, i+1);
+                break; 
+            }
+            printf("Como $j = %d$ no domina a $i = %d$, ir a Paso 3.\n\n", j+1, i+1);
+            if (j < pop_size-1)
+            printf("Paso 3: Como $j = %d \\leq N = %d$, ir a Paso 2.\n\n",j+1, pop_size);
         }
 
         // if i is non-dominated by any in population
-        if (j >= pop_size) 
+        if (j >= pop_size) {
+            printf("Paso 3: Como $j = N = %d$, $P' = P' \\union \\{%d\\}$. ", j,i+1 );
             non_dominated[k++] = i;
+            printf("Así, $P' = \\{");
+            for (l = 0; l < k; ++l) 
+                printf("%d, ", non_dominated[l] +1 );
+            printf("\\}$. Ir a paso 4.\n\n");
+        }
+
+        printf("Paso 4: Incrementando $i$ a $i+1=%d$.\n\n", i+2 );
     }
+
+    printf("Paso 4: Como $i\\geq N$, entonces ");
+    printf("$P' = \\{");
+    for (l = 0; l < k; ++l) 
+        printf("%d, ", non_dominated[l] +1 );
+    printf("\\}$ es el conjunto no dominado.\n\n");
 
     *n_non_dominated = k;
 }
@@ -70,33 +93,71 @@ void continous_update(float* fvalues, int* non_dominated, int* mask, int pop_siz
     int i, j, len_p=1;
     int jj, ii;
     char relation;
+    int l;
 
     // step 1
     non_dominated[0] = mask[0];
+    printf("Paso 1: $P' = \\{ %d \\}, \\ i = 2$.  \n\n", non_dominated[0] + 1);
 
     for (i = 1; i < len_mask; ++i) {
         j = 0;
+        printf("Paso 2: Hacer $j = 1$ . \n \n");
         ii = mask[i];
         while ( j < len_p ) {
             jj = non_dominated[j];
+            printf("Paso 3: Comparar solución $i=%d$ con la $j=%d$ de $P'$.\n\n", ii+1, jj+1);
             relation = compare(&fvalues[ii*n], &fvalues[jj*n], n);
             if(relation == 'D'){
+                printf("Paso 4: Como el miembro $j=%d$ de $P'$ domina a $i=%d$.", jj+1, ii+1 );
+                if(i<len_mask-1)
+                    printf("Incrementar i en uno e ir al Paso 2. \n\n");
+                else
+                printf(" Incrementar $i=%d$ en uno e ir al Paso 5.\n\n", ii+1);
                 break;
             }
 
             if (relation == 'd'){
+                printf("Paso 4: Como $i = %d$ domina a $j=%d$, entonces borrar el elemento %d de $P'$. ", ii+1, jj+1, jj+1);
                 deleteat(non_dominated, len_p, j);
                 jj = non_dominated[j];
                 --len_p;
+
+                printf("Así, $P' = \\{");
+                for (l = 0; l < len_p; ++l) 
+                    printf("%d, ", non_dominated[l] +1 );
+                printf("\\}$. \n\n");
                 continue;
             }
+            if (j<len_p-1) {
+                printf("Paso 4: Como $i$ no domina a $j$ y $j=%d < |P'| = %d$, entonces incrementar $j$ en uno e ir al Paso 3.\n\n", jj+1, len_p);
+            }else
+                printf("Paso 4: Como $i$ no domina a $j$ y $j= |P'| = %d$, entonces ir al Paso 5.\n\n", len_p);
             ++j;
         }
 
         // if i is non-dominated by any in population
-        if (j >= len_p) 
+        if (j >= len_p){
+            //printf("Paso 4: Como $j = %d \\geq |P'|=%d$, ir a Paso 5.\n\n", jj+1, len_p);
+            printf("Paso 5: Insertar $i = %d$ en $P'$. ", ii+1);
             non_dominated[len_p++] = ii;
+
+            printf("Así, $P' = \\{");
+            for (l = 0; l < len_p; ++l) 
+                printf("%d, ", non_dominated[l] +1 );
+            printf("\\}$. ");
+
+            if (i<len_mask-1) {
+                printf("Incrementar $i$ en uno e ir al Paso 2.\n\n");
+            }
+        }
     }
+
+    printf("Paso 5: Como $i\\geq N $, entonces el conjunto no dominado es:\n");
+    printf("$P' = \\{");
+    for (l = 0; l < len_p; ++l) 
+        printf("%d, ", non_dominated[l] +1 );
+    printf("\\}$\n");
+
 
     *n_non_dominated = len_p;
 }
